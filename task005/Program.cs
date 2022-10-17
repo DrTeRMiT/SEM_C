e516c56ca0b1588a93f38df87600f60c1c983fe7
Pavel Zrazheyski <termitspy@gmail.com>
2022-10-17 21:00:57 +0300

    task 05

---
+++ task005/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10, 99);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {num}");
+
+int firstDigit = num / 10; // 78 / 10 = 7
+int secondDigit = num % 10; // 78 % 10 = 8
+
+int res = firstDigit;
+if(secondDigit > firstDigit) res = secondDigit;
+Console.WriteLine($"Наибольшая цифра числа {num} = {res}");
+
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа {num} = {result}");
+
+Console.Write($"Наибольшая цифра числа {num} = ");
+Console.Write(firstDigit > secondDigit ? firstDigit : secondDigit);
